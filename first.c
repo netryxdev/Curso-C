49c34f6c7c17ceb04e13abc4b2a65069ea85ae30
netryxdev <adrianoneto2501@gmail.com>
2021-11-05 04:54:35 -0300

    first commit

---
+++ first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função principal do programa
+int main(){ //Dá pra fazer também com o "void main"
+    //Imprime na tela
+    printf("Hello World");
+    //Retorna o valor 0
+    return 0;
+}
